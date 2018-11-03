aeae123fa2876bbb42e799d4e3b4be681a769072
Jason Ren <renzhzh@gmail.com>
2018-11-02 21:51:45 -0500

    Update test.sh

--- eval-3rd-party/voc_cls/test.sh
+++ eval-3rd-party/voc_cls/test.sh
@@ -5,9 +5,4 @@ CUDA_VISIBLE_DEVICES=6 python3 src/train_cls.py \
     -nit 80000 \
     -lr 0.001 \
     -step 10000 \
-    --scale 0.003921 \
-
-    # # !!! 67.1 : 8w, 0.001 step:1w
-    # #--random-from pool
-    # #--train-from pool5
-#     #--output-dir ./random_conv12/
+    --scale 0.003921
